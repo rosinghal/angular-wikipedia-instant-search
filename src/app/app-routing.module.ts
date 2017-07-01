a1b695ed57b5a921edf47dad9b8009796617e6d6
Rohit Singhal <rohitsinghal.rs@gmail.com>
2017-07-01 12:43:09 +0530

    linking wiki module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    children: []
+    loadChildren: 'app/wikipedia-search/wikipedia-search.module#WikipediaSearchModule'
   }
 ];
 
